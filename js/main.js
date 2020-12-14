8dbebb1b2797f511eec83130794325151d51e144
Luis Silva Martin <72401861+dev-LuisSM@users.noreply.github.com>
2020-12-14 16:01:34 -0600

    Creación de login
    Creación y diseño responsivo de login.

--- js/main.js
+++ js/main.js
@@ -2,12 +2,29 @@ $(document).ready(function () {
     var nav = false;
     $(".fa-bars").click(function () {
         if (nav == false) {
-            $(".ul-links ul").animate({ "left": "4%" });
+            $(".ul-links ul").animate({ "left": "0" });
+            $(".glass").animate({ "left": "0" });
             nav = true;
         }
         else if (nav) {
             $(".ul-links ul").animate({ "left": "-100%" });
+            $(".glass").animate({ "left": "-100%" });
             nav = false;
         }
     });
+
+    var formChng = false;
+    $(".change").click(function() {
+        if(formChng == false){
+            $("#login").animate({"margin":"0 0 0 120%"});
+            $("#registro").animate({"margin":"-7% 0 0 0%"});
+            formChng = true;
+        }
+        else if(formChng){
+            $("#registro").animate({"margin":"-7% 0 0 120%"});
+            $("#login").animate({"margin":"0 0 0 0"});
+            formChng = false;
+        }
+    });
+
 });
