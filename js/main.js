a16e80c62815c623cf424230f8997467239a50ef
Luis Silva Martin <72401861+dev-LuisSM@users.noreply.github.com>
2020-12-15 22:24:52 -0600

    Creación de páginas: Contacto, Nosotros y Política de Privacidad/Términos y Condiciones
    Creación de páginas con diseño responsivo.

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ $(document).ready(function () {
         }
     });
 
+    /* Login - Change form */
     var formChng = false;
     $(".change").click(function() {
         if(formChng == false){
@@ -24,6 +25,63 @@ $(document).ready(function () {
             $("#registro").animate({"margin":"-7% 0 0 120%"});
             $("#login").animate({"margin":"0 0 0 0"});
             formChng = false;
+        }
+    });
+
+    /* Términos y Condiciones - Políticas de Privacidad */
+    $("#pdp-flag").click(function(){
+        if (typeof(Storage) !== "undefined") 
+            localStorage.setItem("page", "tyc");      
+    });
+
+    $("#tyc-flag").click(function(){
+        if (typeof(Storage) !== "undefined") 
+            localStorage.setItem("page", "pdp");
+    });
+
+    var politica;
+    var terminos;
+    if($(".terminos").length){
+        var ls = localStorage.getItem("page");
+        if(ls == "pdp"){
+            $(".ter-con").slideDown(750);
+            terminos = false;
+            politica = true;
+            $(".tc").toggleClass("rotate");
+        }
+        else if(ls == "tyc"){
+            $(".pol-priv").slideDown(750);
+            politica = false;
+            terminos = true;
+            $(".pp").toggleClass("rotate");
+        }
+    }
+
+    $(".pp").click(function(){
+        if(politica == false){
+            $(".pol-priv").slideUp(750);
+            politica = true;
+            $(".pp").removeClass("rotate");
+        }
+        else if(politica){
+            $(".pol-priv").slideDown(750);
+            politica = false;
+            $(".pp").toggleClass("rotate");
+        }
+    });
+
+    $(".tc").click(function(){
+        var rotate = 0; 
+
+        if(terminos == false){
+            $(".ter-con").slideUp(750);
+            terminos = true;
+            $(".tc").removeClass("rotate");
+        }
+        else if(terminos){
+            $(".ter-con").slideDown(750);
+            terminos = false;
+            $(".tc").toggleClass("rotate");
         }
     });
 
