ebcbfac532e3ccb8363841c35581f37426bd284c
Luis Silva Martin <72401861+dev-LuisSM@users.noreply.github.com>
2020-12-23 18:54:23 -0600

    Cambio de slider
    Cambio de slider implementando Slick.js

--- js/main.js
+++ js/main.js
@@ -34,6 +34,27 @@ $(document).ready(function () {
         $("#user div").toggle("slide");
     });
 
+    /* Carrusel */
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
 
 
     /* Términos y Condiciones - Políticas de Privacidad */
